56af434316f4b091dc5b2b997470bfbd29a923bb
mhkoji <mhkoji2+git@gmail.com>
2023-09-02 09:25:32 +0900

    Fix load error

--- tiny-websocket.lisp
+++ tiny-websocket.lisp
@@ -7,17 +7,18 @@
            :taskmaster))
 (in-package :tiny-websocket)
 
-(defun simple-inserter (insert-fn)
-  (lambda (acc next)
-    (if (listp next)
-        (funcall insert-fn acc next)
-        (list next acc))))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun simple-inserter (insert-fn)
+    (lambda (acc next)
+      (if (listp next)
+          (funcall insert-fn acc next)
+          (list next acc))))
 
-(defun insert-first (arg surround)
-  (list* (car surround) arg (cdr surround)))
+  (defun insert-first (arg surround)
+    (list* (car surround) arg (cdr surround)))
 
-(defun insert-last (arg surround)
-  (append surround (list arg)))
+  (defun insert-last (arg surround)
+    (append surround (list arg))))
 
 (defmacro -> (initial-form &rest forms)
   (reduce (simple-inserter #'insert-first) forms
